4fe2df7d2dc4286cc9dd3fd3d722c4566054e8c5
Toitio <josetoitio@gmail.com>
2022-01-26 17:24:10 -0300

    alterando o Table e criand um Filter

--- src/context/Provider.jsx
+++ src/context/Provider.jsx
@@ -1,23 +1,23 @@
 import React, { useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
 import requestAPI from '../services/requestAPI';
-import planets from './planets';
+import planetsContext from './planets';
 
 function Provider({ children }) {
-  const [planetss, setPlanets] = useState([]);
+  const [data, setData] = useState([]);
   useEffect(() => {
     requestAPI()
-      .then(({ results }) => setPlanets(results));
+      .then(({ results }) => setData(results));
   }, []);
 
   const dataContext = {
-    data: planetss,
+    data,
   };
 
   return (
-    <planets.Provider value={ dataContext }>
+    <planetsContext.Provider value={ dataContext }>
       {children}
-    </planets.Provider>
+    </planetsContext.Provider>
   );
 }
 Provider.propTypes = {
--- src/context/planets.jsx
+++ src/context/planets.jsx
@@ -1,5 +1,5 @@
 import { createContext } from 'react';
 
-const planets = createContext();
+const planetsContext = createContext();
 
-export default planets;
+export default planetsContext;
